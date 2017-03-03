c7f1e2c883fb4b9d8759066f273a71a2d53ed332
antton <antton@da5-cs-b221-06.pc.helsinki.fi>
2017-03-03 11:38:39 +0200

    Alueiden nimien järjestäminen aakkojärjestykseen.

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -1,5 +1,6 @@
 package tikape.runko;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -28,6 +29,8 @@ public class Main {
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
             List<Keskustelualue> alueet = keskustelualueDao.etsiKaikki();
+            Collections.sort(alueet);           
+            
             List<Viesti> viimeisimmat = viestiDao.alueidenViimeisimmat(alueet);
             List<Integer> viestimaarat = viestiDao.alueidenViestimaarat(alueet);
             map.put("alueet", alueet);
